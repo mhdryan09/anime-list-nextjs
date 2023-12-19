afb66faec256c305e7726bd6c284e718b082babf
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-12-19 16:21:18 +0700

    Fetching Data Top Anime

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,18 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
-const AnimeList = () => {
+const AnimeList = ({ title, images }) => {
   return (
-    <div className='grid h-32 grid-cols-3 gap-2'>
-      <div className='bg-indigo-500'>
-        <Image src="https://placehold.co/600x400/png" alt='image' width={600} height={400} />
-        <h3>Judul Anime Disini</h3>
-      </div>
-      <div className='bg-indigo-500'>Kotak 2</div>
-      <div className='bg-indigo-500'>Kotak 3</div>
-      <div className='bg-indigo-500'>Kotak 4</div>
-      <div className='bg-indigo-500'>Kotak 5</div>
-      <div className='bg-indigo-500'>Kotak 6</div>
+    <div className='bg-indigo-500'>
+      <Image src={images} alt='image' width={600} height={400} />
+      <h3>{title}</h3>
     </div>
 
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,25 @@
 import AnimeList from "./components";
 
-export default function Home() {
+const Home = async () => {
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`);
+  const animeList = await response.json();
+  // console.log(animeList.data);
+
   return (
     <div>
       <h1>PALING POPULER</h1>
-      <AnimeList />
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-5">
+        {animeList.data.map(anime => {
+          return (
+            <div key={anime.mal_id} className="shadow-xl">
+              <AnimeList title={anime.title} images={anime.images.webp.image_url} />
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
+
+export default Home;
