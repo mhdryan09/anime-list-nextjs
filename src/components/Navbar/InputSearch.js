68ae7ee289f8bd43621d53a78ca8dcbcf249c189
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-12-20 14:01:35 +0700

    Tugas Besar: Membuat Search Input dengan enter pada keyboard

--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -12,14 +12,17 @@ const InputSearch = () => {
 
   // event handler
   const handleSearch = (event) => {
-    event.preventDefault();
-    const keyword = searchRef.current.value;
-    router.push(`/search/${keyword}`);
+    // jika tombol enter ditekan
+    if (event.key === "Enter" || event.type === 'click') {
+      event.preventDefault();
+      const keyword = searchRef.current.value;
+      router.push(`/search/${keyword}`);
+    }
   }
 
   return (
     <div className="relative">
-      <input type="text" placeholder='Search Anime...' className='p-2 rounded w-full' ref={searchRef} />
+      <input type="text" placeholder='Search Anime...' className='p-2 rounded w-full' ref={searchRef} onKeyDown={handleSearch} />
       <button className="absolute top-2 end-2" onClick={handleSearch}>
         <MagnifyingGlass size={24} />
       </button>
