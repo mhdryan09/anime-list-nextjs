9a8e9e6396dc6923bf282a88aa63c8b803b0214f
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-12-21 13:59:35 +0700

    [Fixing]: Fix other minor issues decode URI

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -3,14 +3,15 @@ import Header from "@/components/AnimeList/Header";
 
 const SearchPage = async ({ params }) => {
   const { keyword } = params;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keyword}`);
+  const decodedKeyword = decodeURI(keyword); // apabila ada keyword yg mengandung tanda khusus kita singkirkan
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`);
   const searchAnime = await response.json();
 
   return (
     <>
       {/* anime terpopuler */}
       <section>
-        <Header title={`Pencarian untuk ${keyword}...`} />
+        <Header title={`Pencarian untuk ${decodedKeyword}...`} />
         <AnimeList api={searchAnime} />
       </section>
     </>
