90d2ff3dc084a3295bfd6fa31e0adbd83c1e42cc
Muhammad Ryan Pranata <pranataryan91@gmail.com>
2023-12-21 14:39:11 +0700

    Feature: Lihat Semua dari Page Terpopuler + Pagination

--- src/app/populer/page.jsx
+++ src/app/populer/page.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
+
+import AnimeList from "@/components/AnimeList";
+import HeaderMenu from "@/components/Utils/HeaderMenu";
+import Pagination from "@/components/Utils/Pagination";
+import { useEffect, useState } from "react";
 
 const PopulerPage = () => {
-  return (
-    <div>PopulerPage</div>
-  )
-}
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
 
-export default PopulerPage
+  const fetchData = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`
+    );
+    const data = await response.json();
+    setTopAnime(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
+  return (
+    <>
+      <HeaderMenu title={`ANIME TERPOPULER #${page}`} />
+      <AnimeList api={topAnime} />
+      <Pagination page={page} lastPage={topAnime.pagination?.last_visible_page} setPage={setPage} />
+    </>
+  );
+};
+
+export default PopulerPage;
